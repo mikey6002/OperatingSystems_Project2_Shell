689e51a0cce8265860910a4e9bf99ab67907bb81
mikey6002 <tun32913@temple.edu>
2023-03-14 22:49:21 -0400

    tsc

--- tsch.c
+++ tsch.c
@@ -7,35 +7,45 @@
 #define MAX_INPUT_LENGTH 100
 #define MAX_PATH_LENGTH 1024
 
+//function to exit shell
 void exit_shell() {
     printf("Exiting shell...\n");
     exit(0);
 }
 
+//function that displays help with user type help in command line
 void display_help() {
+printf("Hello and welcome to the help page this is meant to help you work around the shell\n");
 printf("Available commands:\n");
-    printf("help: Display this help message\n");
+    printf("help: Displays this help message\n");
     printf("exit: Exit the shell\n");
     printf("wait: Wait for any child process to terminate\n");
     printf("pwd:Print the current working directory\n");
     printf("cd: Change the current working directory\n");
 }
 
+//blocks calling of rpcoess until on of the chiild processes terminated then returns PID of the terminated child or -1 if no child
 void wait_for_child_process() {
     int status;
+    //wait for child to terminated store exit status 
     pid_t pid = wait(&status);
+    //print PID and exit status of terminated child 
     printf("Process %d terminated with status %d\n", pid, status);
 }
 
 void print_current_working_directory() {
+    //sotre current working directory
     char path[MAX_PATH_LENGTH];
+    //get working direct store it to path 
     if (getcwd(path, MAX_PATH_LENGTH) != NULL) {
+        //print contents of path
         printf("%s\n", path);
+        //error handling
     } else {
         printf("Error: Could not get current working directory\n");
     }
 }
-
+//changes working directory
 void change_directory(char *directory) {
     if (chdir(directory) == 0) {
         printf("Changed directory to %s\n", directory);
@@ -43,7 +53,7 @@ void change_directory(char *directory) {
         printf("Error: Could not change directory to %s\n", directory);
     }
 }
-
+//takes a command and arguement and process the command with the function
 void process_command(char *command, char *argument) {
     if (strcmp(command, "help") == 0) {
         display_help();
@@ -61,15 +71,21 @@ void process_command(char *command, char *argument) {
 }
 
 int main() {
+    printf("Hello and welcome to my shell\n");
+    //user input
     char input[MAX_INPUT_LENGTH];
+    // the command and arguements by user
     char *command, *argument;
-
+    // will keep going until exit 
     while (1) {
+
         printf(">> ");
+        //reads input 
         fgets(input, MAX_INPUT_LENGTH, stdin);
 
-        // tokenize input string
+        //split strings to tokens
         command = strtok(input, " \n");
+        //if no more tokens asigned null
         argument = strtok(NULL, " \n");
 
         if (command != NULL) {
