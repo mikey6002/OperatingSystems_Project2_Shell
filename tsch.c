671cf59e23b52b30ce7bf1267c7e4303704eb575
Barry Lin <tun32913@CIS-Linux2.temple.edu>
2023-03-26 22:35:47 -0400

    shell

--- tsch.c
+++ tsch.c
@@ -2,119 +2,161 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 
+#define MAXLINE 8192
+#define MAXARGS 128
 
-#define MAX_INPUT_LENGTH 100
-#define MAX_PATH_LENGTH 1024
+extern char **environ;
 
-//function to exit shell
-void exit_shell() {
-    printf("Exiting shell...\n");
-    exit(0);
-}
+// Most of the code was from Textbook Bryant Ohallaron  pg 755
+void eval(char *cmdline);
+int parseline(char *buf, char **argv);
+int builtin_command(char **argv);
+void unix_error(char *msg);
+char *Fgets(char *ptr, int n, FILE *stream);
 
-//function that displays help with user type help in command line
-void display_help() {
-    printf("Hello and welcome to the help page this is meant to help you work around the shell\n");
-    printf("Available commands:\n");
-    printf("help: Displays this help message\n");
-    printf("exit: Exit the shell\n");
-    printf("wait: Wait for any child process to terminate\n");
-    printf("pwd:Print the current working directory\n");
-    printf("cd: Change the current working directory\n");
-}
+int main()
+{
+    char cmdline[MAXLINE];
+    while (1)
+    {
+        printf("Hello and Welcome to my Shell!");
+        printf("> ");
+        fflush(stdout);
 
-//blocks calling of rpcoess until on of the chiild processes terminated then returns PID of the terminated child or -1 if no child
-void wait_for_child_process() {
-    int status;
-    //wait for child to terminated store exit status 
-    pid_t pid = wait(&status);
-    //print PID and exit status of terminated child 
-    printf("Process %d terminated with status %d\n", pid, status);
-}
+        Fgets(cmdline, MAXLINE, stdin);
+        if (feof(stdin))
+            exit(0);
 
-void print_current_working_directory() {
-    //sotre current working directory
-    char path[MAX_PATH_LENGTH];
-    //get working direct store it to path 
-    if (getcwd(path, MAX_PATH_LENGTH) != NULL) {
-        //print contents of path
-        printf("%s\n", path);
-        //error handling
-    } else {
-        printf("Error: Could not get current working directory\n");
-    }
-}
-//changes working directory
-//borrowed from slides
-void change_directory(char *directory) {
-    if (chdir(directory) == 0) {
-        printf("Changed directory to %s\n", directory);
-    } else {
-        printf("Error: Could not change directory to %s\n", directory);
-    }
-}
-//takes a command and arguement and process the command with the function
-void process_command(char *command, char *argument) {
-    if (strcmp(command, "help") == 0) {
-        display_help();
-    } else if (strcmp(command, "exit") == 0) {
-        exit_shell();
-    } else if (strcmp(command, "wait") == 0) {
-        wait_for_child_process();
-    } else if (strcmp(command, "pwd") == 0) {
-        print_current_working_directory();
-    } else if (strcmp(command, "cd") == 0) {
-        change_directory(argument);
-    } 
-    else {
-        
+        eval(cmdline);
     }
 }
 
-void find_and_run_wc() {
-    char *path_env = getenv("PATH");
-    char *path = strtok(path_env, ":");
+void eval(char *cmdline)
+{
+    char *argv[MAXARGS];
+    char buf[MAXLINE];
+    int bg;
+    pid_t pid;
 
-    while (path != NULL) {
-        char wc_path[MAX_PATH_LENGTH];
-        snprintf(wc_path, MAX_PATH_LENGTH, "%s/wc", path);
-        if (access(wc_path, X_OK) == 0) {
-            printf("Found wc at %s\n", wc_path);
-            char *argv[] = {wc_path, NULL};
-            execv(wc_path, argv);
+    strcpy(buf, cmdline);
+    bg = parseline(buf, argv);
+    if (argv[0] == NULL)
+        return;
+
+    if (!builtin_command(argv))
+    {
+        if ((pid = fork()) == 0)
+        {
+            if (execve(argv[0], argv, environ) < 0)
+            {
+                printf("%s: Command not found.\n", argv[0]);
+                exit(0);
+            }
         }
-        path = strtok(NULL, ":");
-    }
 
-    printf("Error: Could not find wc in PATH\n");
-}
-
-
-int main() {
-    printf("Hello and welcome to my shell\n");
-    //user input
-    char input[MAX_INPUT_LENGTH];
-    // the command and arguements by user
-    char *command, *argument;
-    // will keep going until exit 
-    while (1) {
-
-        printf(">> ");
-        //reads input 
-        fgets(input, MAX_INPUT_LENGTH, stdin);
-
-        //split strings to tokens
-        command = strtok(input, " \n");
-        //if no more tokens asigned null
-        argument = strtok(NULL, " \n");
-
-        if (command != NULL) {
-            process_command(command, argument);
+        if (!bg)
+        {
+            int status;
+            if (waitpid(pid, &status, 0) < 0)
+                unix_error("waitfg: waitpid error");
+        }
+        else
+        {
+            printf("%d %s", pid, cmdline);
         }
     }
+    return;
+}
 
+int builtin_command(char **argv)
+{
+    if (!strcmp(argv[0], "quit"))
+        exit(0);
+    if (!strcmp(argv[0], "help"))
+    {
+        printf("Type quit to exit the shell.\n");
+        printf("Type program names and arguments, and hit enter.\n");
+        printf("The following are built in:\n");
+        printf("cd [dir] -- change the current working directory\n");
+        printf("pwd -- print the current working directory\n");
+        printf("help -- display this help message\n");
+        return 1;
+    }
+    if (!strcmp(argv[0], "pwd"))
+    {
+        char cwd[MAXLINE];
+        if (getcwd(cwd, sizeof(cwd)) != NULL)
+        {
+            printf("%s\n", cwd);
+        }
+        else
+        {
+            perror("getcwd() error");
+        }
+        return 1;
+    }
+    if (!strcmp(argv[0], "cd"))
+    {
+        if (argv[1] == NULL)
+        {
+            fprintf(stderr, "cd: expected argument to \"cd\"\n");
+        }
+        else if (chdir(argv[1]) != 0)
+        {
+            perror("cd error");
+        }
+        return 1;
+    }
+    if (!strcmp(argv[0], "&"))
+        return 1;
     return 0;
+
+}
+
+int parseline(char *buf, char **argv)
+{
+    char *delim;
+    int argc;
+    int bg;
+
+    buf[strlen(buf) - 1] = ' ';
+    while (*buf && (*buf == ' '))
+        buf++;
+
+    argc = 0;
+    while ((delim = strchr(buf, ' ')))
+    {
+        argv[argc++] = buf;
+        *delim = '\0';
+        buf = delim + 1;
+        while (*buf && (*buf == ' '))
+            buf++;
+    }
+    argv[argc] = NULL;
+
+    if (argc == 0)
+        return 1;
+
+    if ((bg = (*argv[argc - 1] == '&')) != 0)
+        argv[--argc] = NULL;
+        bg =1;
+
+    return bg;
+}
+
+void unix_error(char *msg)
+{
+    fprintf(stderr, "%s\n", msg);
+    exit(1);
+}
+
+char *Fgets(char *ptr, int n, FILE *stream)
+{
+    char *rptr;
+    if (((rptr = fgets(ptr, n, stream)) == NULL) && ferror(stream))
+        unix_error("Fgets error");
+    return (rptr);
 }
